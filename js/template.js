395c97a6537d530d57ef05e7cd60e48a45005e83
jishant07 <jishanta@gmail.com>
2019-09-12 16:47:39 +0530

    Added working sliding

--- js/template.js
+++ js/template.js
@@ -1,10 +1,9 @@
 console.log("CONNECTED");
 
-function a()
+/*function a()
 {
 	alert("YOU CLICKED");
 	var x = document.getElementById('login');
 	console.log(x);
 	x.classList.remove("abc");
-	
-}
+}*/
